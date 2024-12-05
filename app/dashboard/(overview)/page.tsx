1caa5aeb8f5e76d5af281545d496709c0ba0f453
Duong Minh Tam <tam.duong@digicommercegroup.com>
2024-12-05 14:18:29 +0700

    fix eslint

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -17,12 +17,12 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-    const {
-        numberOfInvoices,
-        numberOfCustomers,
-        totalPaidInvoices,
-        totalPendingInvoices,
-      } = await fetchCardData();
+    // const {
+    //     numberOfInvoices,
+    //     numberOfCustomers,
+    //     totalPaidInvoices,
+    //     totalPendingInvoices,
+    //   } = await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
